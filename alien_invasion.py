86f090b75237ff6a1dcf12ab78be1450f1fc3aa7
posada-xabier <posada.xabier.duran@gmail.com>
2020-09-16 11:49:35 +0200

    fondo gris

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,16 +7,18 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("o titulo e ALIEN INVASION ")
+        self.bg_color = (230,230,230)
 
     def run_game(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        self.screen.fill(self.bg_color)    
         pygame.display.flip()
 
 if __name__ == '__main__':
 
     ai = AlienInvasion()
     ai.run_game()
-
 
